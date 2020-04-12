22dab54137cd80428de4decca42fa311ceb15a7b
Tosh Velaga <toshvelaga@gmail.com>
2020-04-12 02:49:02 -0700

    added router and header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+import classes from './Header.css';
+
+const Header = () => {
+    return (
+    <div className={classes.Header}>
+
+    </div>
+    );
+}
+ 
+export default Header;
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header/Header';
+import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return (<>
+            <Header />
+            <HeaderTitle />
+            <h1>Home</h1>
+        </>);
+    }
+}
+ 
+export default Home;
