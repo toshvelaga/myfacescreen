73ce21eaa32fd7abbfeea3d2a1e51d5651f79480
Tosh Velaga <toshvelaga@gmail.com>
2020-04-12 18:15:20 -0700

    styled navbar

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './Hamburger.css'
 
+// codepen: https://codepen.io/designcouch/pen/Atyop
+
+
 const Hamburger = () => {
-    return (
-  <div id="nav-icon1">
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
-  );
+
+    const openNav = () => {
+        // const element = document.getElementById('mySidenav')
+        // ReactDOM.findDOMNode(element).style.width = "250px";
+        alert('hello')
+    }
+
+    return (<>
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
+
+        <div id="nav-icon1" onClick={openNav}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+  </>);
 }
  
 export default Hamburger;
