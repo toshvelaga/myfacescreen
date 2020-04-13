e064f911bd6f8a4e5b43cf62ce64c60ee913715f
Tosh Velaga <toshvelaga@gmail.com>
2020-04-12 23:47:29 -0700

    added GetStarted container

---
+++ src/containers/GetStarted/GetStarted.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GetStarted extends Component {
+    state = {  }
+    render() { 
+        return (<>
+        </>);
+    }
+}
+ 
+export default GetStarted;
