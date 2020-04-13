2d913d8eedbf517b4be84849c177ccad74fd0b42
Tosh Velaga <toshvelaga@gmail.com>
2020-04-12 22:22:20 -0700

    added buttons

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
 import Navbar from '../../components/Navbar/Navbar'
+import HeroImageHome from '../../components/HeroImageHome/HeroImageHome';
+import HomeDividerTwo from '../../components/HomeDividerTwo/HomeDividerTwo';
+import OrangeBtn from '../../components/OrangeBtn/OrangeBtn'
 
 class Home extends Component {
     state = {  }
@@ -9,8 +12,10 @@ class Home extends Component {
         return (<>
             <Header />
             <HeaderTitle />
-            {/* <h1>Home</h1> */}
             <Navbar />
+            <HeroImageHome />
+            <HomeDividerTwo />
+            <OrangeBtn text="Take the Quiz"/>
         </>);
     }
 }
