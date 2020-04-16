5d5fe769d806cb4f358213bbf742dbb2aff12ab9
Tosh Velaga <toshvelaga@gmail.com>
2020-04-16 02:12:56 -0700

    added firebase, firestore, fbconfig, as well as login page

--- src/index.js
+++ src/index.js
@@ -4,10 +4,52 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './store/reducers/rootReducer';
+import thunk from 'redux-thunk'
+import { reduxFirestore ,getFirestore, createFirestoreInstance } from 'redux-firestore'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import fbConfig from './firebase/fbConfig'
+import firebase from "firebase/app";
+
+import { useSelector  } from 'react-redux'
+import { isLoaded  } from 'react-redux-firebase';
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+    reduxFirestore(fbConfig),
+  )
+);
+
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+  userProfile: 'users', // where profiles are stored in database
+  presence: 'presence', // where list of online users is stored in database
+  sessions: 'sessions'
+};
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <></>;
+      return children
+}
+
 ReactDOM.render(
+  <Provider store={store}>
+  <ReactReduxFirebaseProvider {...rrfProps}>
+    <AuthIsLoaded>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </AuthIsLoaded>
+      </ReactReduxFirebaseProvider>
+    </Provider>,
   document.getElementById('root')
 );
 
