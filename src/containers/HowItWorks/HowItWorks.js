02034ccd4ba7ca6e0f634e9f83b3aa1e2a7fd9b6
Tosh Velaga <toshvelaga@gmail.com>
2020-04-22 02:14:40 -0700

    added grid to how it works

--- src/containers/HowItWorks/HowItWorks.js
+++ src/containers/HowItWorks/HowItWorks.js
@@ -3,6 +3,10 @@ import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
 import Navbar from '../../components/Navbar/Navbar'
 import Grid from '@material-ui/core/Grid';
 import OrangeButton from '../../components/Buttons/OrangeButton/OrangeButton'
+import Hr from '../../components/Hr/Hr'
+import Footer from '../../components/Footer/Footer'
+
+import './HowItWorks.css'
 
 class HowItWorks extends Component {
     state = {  }
@@ -10,10 +14,37 @@ class HowItWorks extends Component {
         return (<>
         <HeaderTitle />
         <Navbar />
-        <hr style={{width: '100%', height: '1px', border: '0', backgroundColor: '#eeeae6'}}/>
-        <h1 style={{color: '#55473e'}}>How It Works</h1>
-        <p style={{color: '#a59b98'}}>Our goal is to craft the highest quality protective face masks<br /> for you and the ones you care about.</p>
-        <OrangeButton text="Get Started"/>
+        <div className="HowItWorksContainer">
+            <Hr />
+            <h1 style={{color: '#55473e'}}>How It Works</h1>
+            <p style={{color: '#a59b98'}}>Our goal is to craft the highest quality protective face masks<br /> for you and the ones you care about.</p>
+            <OrangeButton text="Get Started"/>
+
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <h4>Get Started</h4>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <h4>We Got You Covered</h4>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <h4>BUT WAIT, THERE'S MORE!</h4>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <h4>YOU SAY WHEN</h4>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <h4>NO LONG-TERM COMMITMENTS</h4>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <h4>30-DAY MONEY BACK GUARANTEE</h4>
+                </Grid>
+            </Grid>
+            <Hr />
+            <OrangeButton text="Get Started" />
+            <Footer />
+
+        </div>
         </>);
     }
 }
