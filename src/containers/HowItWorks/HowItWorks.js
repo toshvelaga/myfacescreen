ea8e11883d31f063ad39c6221c5ead546b858d57
Tosh Velaga <toshvelaga@gmail.com>
2020-04-22 00:42:39 -0700

    changed styles of buttons

--- src/containers/HowItWorks/HowItWorks.js
+++ src/containers/HowItWorks/HowItWorks.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
 import Navbar from '../../components/Navbar/Navbar'
+import Grid from '@material-ui/core/Grid';
+import OrangeButton from '../../components/Buttons/OrangeButton/OrangeButton'
 
 class HowItWorks extends Component {
     state = {  }
@@ -9,7 +11,9 @@ class HowItWorks extends Component {
         <HeaderTitle />
         <Navbar />
         <hr style={{width: '100%', height: '1px', border: '0', backgroundColor: '#eeeae6'}}/>
-        <h1>How It Works</h1>
+        <h1 style={{color: '#55473e'}}>How It Works</h1>
+        <p style={{color: '#a59b98'}}>Our goal is to craft the highest quality protective face masks<br /> for you and the ones you care about.</p>
+        <OrangeButton text="Get Started"/>
         </>);
     }
 }
