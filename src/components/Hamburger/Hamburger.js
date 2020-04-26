9bb525df5bb27184f91f66a348c051c16a3d1291
Tosh Velaga <toshvelaga@gmail.com>
2020-04-25 22:37:07 -0700

    got navbar to not warp links and text on close

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -3,6 +3,7 @@ import './Hamburger.css'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signOut } from '../../store/actions/actions'
+import usaflag from '../../assets/usa-flag.svg'
 
 // codepen: https://codepen.io/designcouch/pen/Atyop
 
@@ -15,9 +16,19 @@ const Hamburger = (props) => {
         <Link to="/get-started">GET STARTED</Link>
         <Link to="/how-it-works">HOW IT WORKS</Link>
         <Link to="/products">OUR PRODUCTS</Link>
+        <hr/>
         <Link onClick={props.signOut} to="/login">LOGOUT</Link>
         <Link to="/join">JOIN</Link>
         <Link to="/box">BOX</Link>
+        <hr/>
+        <section>
+            <div style={{ verticalAlign: 'middle' }}>
+                <img src={usaflag} style={{width: '35px', height: '35px'}} />
+            </div>
+            <div style={{verticalAlign: 'middle' }}>
+                <p style={{color: '#b9b9b9', marginLeft: '1rem', fontWeight: 'bold', whiteSpace: 'nowrap'}}>MADE IN AMERICA</p>
+            </div>    
+        </section>
     </div>
 
     <div id="nav-icon1" onClick={props.openNav}>
